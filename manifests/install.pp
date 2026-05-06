7f39343eb14690384fb3cae9459b5083eccd15a5
Mick Timony <michael_timony@hms.harvard.edu>
2026-05-06 05:00:51 -0400

    update to use python3-pip instead of python-pip

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,7 +10,7 @@ class diamond::install {
         # include epel
         # ensure_resource('package', 'python-pip', { 'ensure' => 'present', 'before' => Package['diamond'], 'require' => Yumrepo['epel'] })
         # ensure_resource('package', ['python-configobj','gcc','python-devel'], { 'ensure' => 'present', 'before' => Package['diamond'], 'require' => Package['python-pip'] })
-        ensure_resource('package', ['python-configobj','python-devel'], { 'ensure' => 'present', 'before' => Package['diamond'], 'require' => Package['python-pip'] })
+        ensure_resource('package', ['python-configobj','python-devel'], { 'ensure' => 'present', 'before' => Package['diamond'], 'require' => Package['python3-pip'] })
       }
       /^(Debian|Ubuntu)$/: {
         ensure_packages(['python-pip','python-configobj','gcc','python-dev'], { 'ensure' => 'present' })
